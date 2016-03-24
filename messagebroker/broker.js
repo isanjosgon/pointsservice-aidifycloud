5e32117994f34a7acc73277f70283e6e36932547
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-25 00:45:56 +0100

    Connect service to message broker

---
+++ messagebroker/broker.js
@@ -0,0 +1,41 @@
+// Created by Isra San Jose Gonzalez
+// @aidify 24/03/2016
+
+'use strict'
+
+const _ = require('lodash');
+const redis = require('redis');
+
+class Broker
+{
+  constructor (config,logger,getstatscore) {
+    if (config.host) {
+      this.client_sub = redis.createClient(config.port,config.host);
+      this.client_sub.auth(config.auth);
+      this.client_pub = redis.createClient(config.port,config.host);
+      this.client_pub.auth(config.auth);
+    } else {
+      this.client_sub = redis.createClient();
+      this.client_pub = redis.createClient();
+    }
+    this.client_sub.subscribe('SERVICE:POINTSSERVICE');
+    this.client_sub.on('message',function (channel,message) {
+      let service = channel.split(':')[1];
+      let action = message.split(':')[0];
+      if (service == 'POINTSSERVICE' && action === 'SET_POINTS') {
+        logger && logger.log('request MESSAGE : ' + message);
+        getstatscore.execute(JSON.parse(message.split(':')[1]),function (err,scores) {
+          if (err) {
+            return logger.error(err);
+          }
+          _.each(scores,function (stat) {
+            logger && logger.log('publish to SERVICE:STATSMANAGEMENT:' + JSON.stringify(stat));
+            client_pub.publish('SERVICE:STATSMANAGEMENT',JSON.stringify(stat));
+          });
+        });
+      }
+    });
+  }
+}
+
+module.exports = Broker;
