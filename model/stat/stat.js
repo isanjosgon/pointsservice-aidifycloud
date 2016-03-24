c087bf72f7f62010053c2c8f5ace0535acdb7e25
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-25 00:46:10 +0100

    Add Stat model

---
+++ model/stat/stat.js
@@ -0,0 +1,15 @@
+// Created by Isra San Jose Gonzalez
+// @aidify 24/03/2016
+
+'use strict'
+
+class Stat
+{
+  constructor (id,stat,points) {
+    this.userid = id;
+    this.stat = stat;
+    this.points = points;
+  }
+}
+
+module.exports = Stat;
